409c988e941259bb2cbbe7082d1a8e7af3179613
NaluFigueira <anachfigueira@gmail.com>
2020-04-26 22:18:18 -0300

    Creating transactions and categories table

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,52 +8,10 @@ interface Balance {
   total: number;
 }
 
-interface CreateTransactionDTO {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-}
-
-class TransactionsRepository {
-  private transactions: Transaction[];
-
-  constructor() {
-    this.transactions = [];
-  }
-
-  public all(): Transaction[] {
-    return this.transactions;
-  }
-
-  public getBalance(): Balance {
-    const income = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'income') return total + transaction.value;
-      return total;
-    }, 0);
-
-    const outcome = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'outcome') return total + transaction.value;
-      return total;
-    }, 0);
-
-    const total = income - outcome;
-    return {
-      income,
-      outcome,
-      total,
-    };
-  }
-
-  public create({ title, value, type }: CreateTransactionDTO): Transaction {
-    const transaction = new Transaction({
-      title,
-      value,
-      type,
-    });
-
-    this.transactions.push(transaction);
-
-    return transaction;
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    // TODO
   }
 }
 
