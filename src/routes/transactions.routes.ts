c4f17c0f494805874f1adc6f157fd7663e00b440
NaluFigueira <anachfigueira@gmail.com>
2020-04-27 01:08:43 -0300

    Adding import transactions csv route

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
@@ -40,8 +43,18 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(200).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const filePath = request.file.path;
+
+    const importTransactionService = new ImportTransactionsService();
+
+    const transactions = await importTransactionService.execute(filePath);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
