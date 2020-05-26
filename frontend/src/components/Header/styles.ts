a86deccc54871e8941a1993d30c515bd4d4a5982
NaluFigueira <anachfigueira@gmail.com>
2020-05-25 23:56:51 -0300

    Adding items for header

--- frontend/src/components/Header/styles.ts
+++ frontend/src/components/Header/styles.ts
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 interface ContainerProps {
   size?: 'small' | 'large';
+}
+
+interface HeaderItemProps {
+  selected?: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -15,22 +20,26 @@ export const Container = styled.div<ContainerProps>`
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
+`;
 
-    nav {
-      a {
-        color: #fff;
-        text-decoration: none;
-        font-size: 16px;
-        transition: opacity 0.2s;
+export const HeaderItem = styled(Link)<HeaderItemProps>`
+  color: #fff;
+  text-decoration: none;
+  font-size: 16px;
+  transition: opacity 0.2s;
+  ${props =>
+    props.selected &&
+    css`
+      padding-bottom: 10px;
+      border-bottom: 2px solid #ff872c;
+    `}
 
-        & + a {
-          margin-left: 32px;
-        }
+  & + a {
+    margin-left: 32px;
+  }
 
-        &:hover {
-          opacity: 0.6;
-        }
-      }
-    }
+  &:hover {
+    opacity: 0.6;
   }
 `;
