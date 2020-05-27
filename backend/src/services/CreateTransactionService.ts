070e4653aedbbf2bf40ddf539c64fadeb0e475e3
NaluFigueira <anachfigueira@gmail.com>
2020-05-26 22:32:32 -0300

    Separating frontend and backend folders

---
+++ backend/src/services/CreateTransactionService.ts
@@ -0,0 +1,56 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+      if (value + balance.outcome > balance.income)
+        throw new AppError('Invalid outcome value');
+    }
+
+    let categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      categoryExists = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryExists);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryExists,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
